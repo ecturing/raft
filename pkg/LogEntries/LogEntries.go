337309c7b0beed200a3430e6c39a5b5a5f97e50f
ecturing <ecturing@vip.qq.com>
2024-05-13 23:37:23 +0800

    日志实例修改

--- pkg/LogEntries/LogEntries.go
+++ pkg/LogEntries/LogEntries.go
@@ -5,9 +5,9 @@ type Logs interface {
 }
 
 type LogsEntry struct {
-	index    int      //当前日志最新索引
-	term     int      //当前日志所属任期号
-	metaData []string //当前日志元数据
+	Index    int    //当前日志最新索引
+	Term     uint   //当前日志所属任期号
+	MetaData string //当前日志元数据
 }
 
 // persist : 持久化日志
@@ -16,6 +16,6 @@ func (l *LogsEntry) persist() {
 }
 
 func (l *LogsEntry) AppendLog(log string) {
-	l.metaData = append(l.metaData, log)
-	l.index++
+	l.MetaData = log
+	l.Index++
 }
